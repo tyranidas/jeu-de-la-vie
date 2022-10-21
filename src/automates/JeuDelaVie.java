5d30449f96c0959acfaca7d65cf978bdd0c18e8e
Manuel <tyranidas@hotmail.com>
2022-10-21 16:51:18 +0200

    rajout bord de terrain

--- src/automates/JeuDelaVie.java
+++ src/automates/JeuDelaVie.java
@@ -8,121 +8,85 @@ public class JeuDelaVie {
 		int colonne = 20;
 
 		int[][] map = new int[ligne][colonne];
+
 		map[15][5] = 1;
 		map[15][6] = 1;
 		map[15][7] = 1;
 		map[16][7] = 1;
 		map[17][6] = 1;
-		
-	
-	
+
 		for (int c = 0; c < map.length; c++) {
 			for (int l = 0; l < map[c].length; l++) {
-				
-			
+
+				if (c == 0 || c == map.length - 1 || l == 0 || l == map[c].length - 1) {
+					map[c][l] = 4;
+				}
 
 				System.out.print(map[c][l] + "|");
 			}
 			System.out.println();
 		}
 		System.out.println();
-			
-		
-		for (int i = 0; i < 10; i++) {
-			 map = generation(map, colonne, ligne);
-		}
-		
-	
 
-	//	int[][] map2 = generation(map1, colonne, ligne);
+		for (int i = 0; i < 10; i++) {
+			map = generation(map, colonne, ligne);
+		}
+
+		// int[][] map2 = generation(map1, colonne, ligne);
 	}
 
 	public static int[][] generation(int[][] map, int colonne, int ligne) {
-		
-		
+
 		int[][] map1 = new int[ligne][colonne];
-		
-	
+
 		for (int c = 0; c < map1.length; c++) {
 			for (int l = 0; l < map1[c].length; l++) {
-	
-				if (l > 0 && l < colonne-1  && c > 0 && c < ligne-1 ) {
 
-					if (
-							(map[c - 1][l - 1]) + 
-							(map[c - 1][l]) + 
-							(map[c - 1][l + 1]) + 
-							(map[c][l - 1]) +
-							(map[c][l + 1]) + 
-							(map[c + 1][l - 1]) + 
-							(map[c + 1][l]) + 
-							(map[c + 1][l + 1]) 
-							== 3)
-					 {
+				if (map[c][l] != 4) {
+
+					if ((map[c - 1][l - 1]) + (map[c - 1][l]) + (map[c - 1][l + 1]) + (map[c][l - 1]) + (map[c][l + 1])
+							+ (map[c + 1][l - 1]) + (map[c + 1][l]) + (map[c + 1][l + 1]) == 3) {
 						map1[c][l] = 1;
-					//	System.out.println(x);
-					} 
-					else if (
-							(map[c - 1][l - 1]) + 
-							(map[c - 1][l]) + 
-							(map[c - 1][l + 1]) + 
-							(map[c][l - 1]) +
-							(map[c][l + 1]) + 
-							(map[c + 1][l - 1]) + 
-							(map[c + 1][l]) + 
-							(map[c + 1][l + 1]) 
-							== 2)
-							{
+						// System.out.println(x);
+					} else if ((map[c - 1][l - 1]) + (map[c - 1][l]) + (map[c - 1][l + 1]) + (map[c][l - 1])
+							+ (map[c][l + 1]) + (map[c + 1][l - 1]) + (map[c + 1][l]) + (map[c + 1][l + 1]) == 2) {
 						map1[c][l] = map[c][l];
+					} else if (((map[c - 1][l - 1]) + (map[c - 1][l]) + (map[c - 1][l + 1]) + (map[c][l - 1])
+							+ (map[c][l + 1]) + (map[c + 1][l - 1]) + (map[c + 1][l]) + (map[c + 1][l + 1]) >= 4)
+							|| ((map[c - 1][l - 1]) + (map[c - 1][l]) + (map[c - 1][l + 1]) + (map[c][l - 1])
+							+ (map[c][l + 1]) + (map[c + 1][l - 1]) + (map[c + 1][l])+ (map[c + 1][l + 1]) < 2))
+
+					{
+						map1[c][l] = 0;
 					}
-					else if (
-							(map[c - 1][l - 1]) + 
-							(map[c - 1][l]) + 
-							(map[c - 1][l + 1]) + 
-							(map[c][l - 1]) +
-							(map[c][l + 1]) + 
-							(map[c + 1][l - 1]) + 
-							(map[c + 1][l]) + 
-							(map[c + 1][l + 1]) 
-							== 4){
-						map1[c][l] =0;
-					}
-					}
+				} else {
+					map1[c][l] = 4;
+				}
 
 				System.out.print(map1[c][l] + "|");
 			}
 			System.out.println();
 		}
 		System.out.println();
-		
-	
-		
+
 		return map1;
-		
+
 	}
 
-}	/*
-int x = map[c][l];
-int ouest = map[c-1][l];
-int nord = map[c][l-1];
-int est = map[c+1][l];
-int sud = map[c-1][l];
-int somme = ouest + nord + est + sud;
-if (x==1)
-{
-	System.out.println("test= "+ somme) ;
-}*/
+} /*
+	 * int x = map[c][l]; int ouest = map[c-1][l]; int nord = map[c][l-1]; int est =
+	 * map[c+1][l]; int sud = map[c-1][l]; int somme = ouest + nord + est + sud; if
+	 * (x==1) { System.out.println("test= "+ somme) ; }
+	 */
 
-/*if (c > 0 && c < colonne-1  && l > 0 && l < ligne-1 ) {
-
-int x = map[c][l];
-
-if (x == 1 && (((map[c - 1][l]) + (map[c + 1][l]) + (map[c][l - 1]) + (map[c][l + 1]) == 2)
-		|| ((map[c - 1][l]) + (map[c + 1][l]) + (map[c][l - 1]) + (map[c][l + 1]) == 2))) {
-	
-	x = 1;
-	System.out.println(x);
-} else {
-	map[c][l] = 0;
-}
-}*/
+/*
+ * if (c > 0 && c < colonne-1 && l > 0 && l < ligne-1 ) {
+ * 
+ * int x = map[c][l];
+ * 
+ * if (x == 1 && (((map[c - 1][l]) + (map[c + 1][l]) + (map[c][l - 1]) +
+ * (map[c][l + 1]) == 2) || ((map[c - 1][l]) + (map[c + 1][l]) + (map[c][l - 1])
+ * + (map[c][l + 1]) == 2))) {
+ * 
+ * x = 1; System.out.println(x); } else { map[c][l] = 0; } }
+ */
